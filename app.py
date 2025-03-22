8b071178dbdebee0fdd5a93ede9cf113d27ce7e6
Marcos <marcxstx@gmail.com>
2025-03-22 16:27:59 +0000

    Add CORS middleware to FastAPI application

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "API ok!", "version": "1.0.0"}
