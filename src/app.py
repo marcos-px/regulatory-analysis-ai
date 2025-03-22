1b240a87be7454e6fa8ba369c7825a2719f241dc
Marcos <marcxstx@gmail.com>
2025-03-22 17:16:03 +0000

    Refactor FastAPI application structure and update dependencies

--- src/app.py
+++ src/app.py
@@ -26,4 +26,5 @@ app.add_middleware(
 async def root():
     return {"message": "API ok!", "version": "1.0.0"}
 
-uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
